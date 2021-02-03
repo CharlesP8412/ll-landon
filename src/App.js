0c0dd958c377d19b5045191dabe3008f4cefdb0d
Charles Prystupa <Charles.Prystupa@gmail.com>
2021-02-02 20:47:15 -0500

    Test Change

--- src/App.js
+++ src/App.js
@@ -15,8 +15,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React on react
         </a>
+        <p> Lynda learning is also good</p>
       </header>
     </div>
   );
